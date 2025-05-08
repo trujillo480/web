633e8d3b50dcbda68338f7626eeac65d05a5d4a1
Connect404 <michaeldanielperaltaquinon@gmail.com>
2025-05-08 18:36:01 -0500

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,30 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Servidor Socket.IO en puerto 3000');
+});
