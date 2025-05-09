ae681647a6ef91d12e7ba78982e3d547abda1521
Connect404 <michaeldanielperaltaquinon@gmail.com>
2025-05-09 15:40:21 -0500

    Update emoji.js

--- public/emoji.js
+++ public/emoji.js
@@ -2,13 +2,15 @@ const emojiPicker = document.getElementById('emoji-picker');
 const toggleEmojiBtn = document.getElementById('toggle-emoji-picker');
 const input = document.getElementById('input');
 
-emojiPicker.style.display = 'none';
+if (emojiPicker && toggleEmojiBtn && input) {
+  emojiPicker.style.display = 'none';
 
-toggleEmojiBtn.addEventListener('click', () => {
-  emojiPicker.style.display = emojiPicker.style.display === 'none' ? 'block' : 'none';
-});
+  toggleEmojiBtn.addEventListener('click', () => {
+    emojiPicker.style.display = emojiPicker.style.display === 'none' ? 'block' : 'none';
+  });
 
-emojiPicker.addEventListener('emoji-click', event => {
-  input.value += event.detail.unicode;
-  input.focus();
-});
+  emojiPicker.addEventListener('emoji-click', event => {
+    input.value += event.detail.unicode;
+    input.focus();
+  });
+}
